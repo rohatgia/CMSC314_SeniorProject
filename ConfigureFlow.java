58c5e5e10bb1fc4ceb680701e9f8046b3fb8357b
harrisbm3 <harrisbm3@vcu.edu>
2017-10-26 18:34:11 -0400

    added Configure Flow

---
+++ ConfigureFlow.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ConfigureFlow {
+
+	ConfigureFlow(){
+		
+	}
+	
+    public void displayMenu(){
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Please enter a menu option: \n" +
+                           "1) Current selected flow module \n"+
+                           "2) Add new module to flow \n"+
+                           "3) Remove selected Module from flow \n");
+        int userChoice = userIn.nextInt();
+        
+        switch (userChoice){
+        case 1:
+        	
+        	break;
+        case 2:
+        	//add new flow menu
+        	break;
+        	
+        case 3:
+        	//Remove selected module from flow
+        	break;
+        }
+    }
+    
+}
