6ab004217d1207c2e923154660752d307409879a
harrisbm3 <harrisbm3@vcu.edu>
2017-10-26 15:31:18 -0400

    Added ConfigureStream menu

---
+++ ConfigureStream.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ConfigureStream {
+
+	ConfigureStream(){
+		displayMenu();
+	}
+	
+    public void displayMenu(){
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Please enter a menu option: \n" +
+                           "1) Add Stream \n" +
+                           "2) Edit Stream \n" +
+                           "3) Export Stream \n");
+        int userChoice = userIn.nextInt();
+        
+        switch (userChoice){
+        case 1:
+        	//execute Add Stream Menu
+        	break;
+        case 2:
+        	//execute Edit Stream Menu
+        	break;
+        case 3:
+        	//execute Export Stream Menu
+        	break;
+    
+        }
+    }
+}
