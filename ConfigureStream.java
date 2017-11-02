c5e2f8727abca0267f85d1ade7e0e54c325fc982
harrisbm3 <harrisbm3@vcu.edu>
2017-11-02 13:24:38 -0400

    menu cleaning more

--- ConfigureStream.java
+++ ConfigureStream.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class ConfigureStream {
 	public static EditStream editStream= new EditStream();
-	public static ExportStream exportStream = new ExportStream();
 	//we will need an arraylist of streams that is static here
 	//we will need a stream variable that represents the current selected stream, this variable will be useful/accesible in many menus
 	
@@ -29,9 +28,8 @@ public class ConfigureStream {
         	editStream.displayMenu();
         	break;
         case 3:
-        	exportStream.displayMenu();
+        	//export stream Tool
         	break;
-    
         }
     }
 }
