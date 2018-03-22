cbb1f197f3936bf4f68e72042b080d38f6cc97ce
wilsonjj <wilsonjj2@vcu.edu>
2018-03-22 04:40:04 -0400

    Added "Edit Ensemble" to menu. Includes an array of a custom pair object for storing parameters. Also includes public get methods for ensemble status (boolean) and ensemble parameters (integers). Also includes some input validation (just don't type a string where an integer should be - it will crash). Also notice I am using the raw version of my custom object - ran into some errors with declaring an array of generic types so I forced it. Dirty code but it works.

--- LearnOptions.java
+++ LearnOptions.java
@@ -11,7 +11,15 @@ public class LearnOptions {
 	public AbstractClassifier currentSelected;
 	public static ArrayList <AbstractClassifier> classifiers = new ArrayList<AbstractClassifier>(); //arraylist of classifiers currently supported by our program
 	public static ArrayList <AbstractClassifier> userSelectedClassifiers= new ArrayList<AbstractClassifier>(); //arraylist of the user selected algorithms that they want in their workspace
-	
+
+
+	//create ensemble options with default values
+	private static Pair<String,String> option0 = new Pair("Enabled","true");
+	private static Pair<String,String> option1 = new Pair("Chunk Size","300");
+	private static Pair<String,String> option2 = new Pair("Window Size","10");
+	private static Pair<String,String> option3 = new Pair("Ensemble Pool","20");
+	private static Pair[] ensembleOptions = {option0,option1,option2,option3};
+
 	LearnOptions(){
 		classifiers.add(new moa.classifiers.bayes.NaiveBayes());
 		classifiers.add(new moa.classifiers.trees.HoeffdingAdaptiveTree());
@@ -39,15 +47,17 @@ public class LearnOptions {
     	}
         Scanner userIn = new Scanner(System.in);
         System.out.println("Please enter a menu option:");
+
         if(currentSelected!=null){
         	System.out.println("1) Selected Algorithm: " + currentSelected.getClass().toString());
         }
-        System.out.println("2) Add Algorithm to workspace \n" +
-                           "3) Edit Parameters");
+        System.out.println("2) Add Algorithm to workspace");
+        System.out.println("3) Edit Parameters");
+        System.out.println("4) Edit Ensemble");
         if(currentSelected!=null){
-            System.out.println("4) Remove selected algorithm");
+            System.out.println("5) Remove selected algorithm");
         }
-    	System.out.println("101) go back \n");
+    	System.out.println("101) go back");
         
         int userChoice = userIn.nextInt();
         
@@ -63,7 +73,10 @@ public class LearnOptions {
         case 3:
         	editParameters(userIn,0);
         	break;
-        case 4:
+		case 4:
+			editEnsemble(userIn, 0);
+			break;
+        case 5:
         	if(currentSelected!=null){
         		userSelectedClassifiers.remove(currentSelected);
             	if(!userSelectedClassifiers.isEmpty()){
@@ -172,7 +185,6 @@ public class LearnOptions {
     			System.out.println(optionArr[i].getName() + "  :   "+optionArr[i].getValueAsCLIString());
     		}
     	}
-    	
     	System.out.println("press 'e' to select or 'w'/'s' to scroll and \"exit\" to go back");
     	String input= userIn.next();
     	switch (input){
@@ -200,4 +212,98 @@ public class LearnOptions {
     		editParameters(userIn,selectionIdx);
     	}
     }
+
+	public void editEnsemble(Scanner userIn, int selection) {
+		boolean exit = false;
+		int selectionIdx = selection;
+
+		for (int i = 0; i < ensembleOptions.length; i++) {
+			if (i == selectionIdx) {
+				System.out.println(ensembleOptions[i].getKey() + "  :   " + ensembleOptions[i].getValue() + "  <<<");
+			} else {
+				System.out.println(ensembleOptions[i].getKey() + "  :   " + ensembleOptions[i].getValue());
+			}
+		}
+		System.out.println("press 'e' to select or 'w'/'s' to scroll and \"exit\" to go back");
+		String input = userIn.next();
+		switch (input) {
+			case "e":
+				System.out.println("Please enter a value for  " + ensembleOptions[selectionIdx].getKey());
+				switch (selectionIdx) {
+					case 0:
+						String temp = userIn.next();
+						if (temp.equals("false") || temp.equals("true")){
+							ensembleOptions[selectionIdx].setValue(temp);
+						} else {
+							System.out.println("\nError: Please type \"true\" or \"false\"");
+						}
+					break;
+					case 1:
+						ensembleOptions[selectionIdx].setValue(userIn.next());
+					break;
+					case 2:
+						String window = userIn.next();
+						String ensemblePool = ensembleOptions[3].getValue().toString();
+						if (Integer.valueOf(window)<=Integer.valueOf(ensemblePool)){
+							ensembleOptions[selectionIdx].setValue(userIn.next());
+						} else {
+							System.out.println("\nError: Please type a number less than or equal to ensemble pool");
+						}
+					break;
+					case 3:
+						String window2 = ensembleOptions[2].getValue().toString();
+						String ensemblePool2 = userIn.next();
+						if (Integer.valueOf(ensemblePool2)>=Integer.valueOf(window2)){
+							ensembleOptions[selectionIdx].setValue(userIn.next());
+						} else {
+							System.out.println("\nError: Please type a number greater then or equal to window size");
+						}
+					break;
+				}
+			case "w":
+				if (selectionIdx > 0) {
+					selectionIdx--;
+				}
+				break;
+			case "s":
+				if (selectionIdx < ensembleOptions.length - 1) {
+					selectionIdx++;
+				}
+				break;
+			case "exit":
+				exit = true;
+				break;
+		}
+		System.out.println();
+
+		if (!exit) {
+			editEnsemble(userIn, selectionIdx);
+		}
+	}
+	public boolean getEnsembleStatus(){
+		if (ensembleOptions[2].getValue().toString().equals("true")){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public int[] getEnsembleParameters(){
+		int[] parameters = {Integer.valueOf(ensembleOptions[1].getValue().toString()),
+							Integer.valueOf(ensembleOptions[2].getValue().toString()),
+							Integer.valueOf(ensembleOptions[3].getValue().toString())};
+		return parameters;
+	}
+}
+
+class Pair<K,V> {
+	private K key;
+	private V value;
+	public Pair(K key, V value){
+		this.key = key;
+		this.value = value;
+	}
+	public K getKey(){return key;}
+	public V getValue(){return value;}
+	public void setKey(K key){this.key = key;}
+	public void setValue(V value){this.value = value;}
 }
